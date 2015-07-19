ecc0f7a83ee2e1146f18b9e811a2e011f49ecf15
Michael Williams <dinosaur@riseup.net>
2015-07-20 02:36:13 +1200

    Updates

--- example.js
+++ example.js
@@ -3,28 +3,20 @@ var main = require('main-loop')
 var readAudio = require('read-audio')
 var CBuffer = require('CBuffer')
 var writable = require('writable2')
-var nextTick = require('next-tick')
 var h = require('virtual-hyperscript-svg')
+
+var rainbowGradient = require('rainbow-linear-gradient')
+var linearGradientToVsvg = require('linear-gradient-svg')
 
 var Scope = require('./')
 
-var scope = Scope({
-  stroke: h('linearGradient', {
-    x1: '0%',
-    y1: '0%',
-    x2: '100%',
-    y2: '0%'
-  }, [
-    h('stop', { offset: '0%', 'stop-color': 'red' }),
-    h('stop', { offset: '14%', 'stop-color': 'orange' }),
-    h('stop', { offset: '28%', 'stop-color': 'yellow' }),
-    h('stop', { offset: '42%', 'stop-color': 'green' }),
-    h('stop', { offset: '56%', 'stop-color': 'blue' }),
-    h('stop', { offset: '70%', 'stop-color': 'indigo' }),
-    h('stop', { offset: '84%', 'stop-color': 'violet' }),
-    h('stop', { offset: '100%', 'stop-color': 'red' })
-  ])
-})
+var opts = {
+  buffer: 1024,
+  channels: 1,
+  inc: 1
+}
+
+var scope = Scope()
 
 var loop = main(
   null,
@@ -32,15 +24,11 @@ var loop = main(
   vdom
 )
 
-var opts = {
-  buffer: 1024,
-  channels: 1
-}
-
 readAudio(opts, function (err, stream) {
   if (err) { throw err }
 
-  var c = 0
+  var start = 0
+  var inc = opts.inc
   var bufShape = [opts.buffer * 10, opts.channels]
   var buf = CBuffer(size(bufShape))
   buf.fill(0)
@@ -52,13 +40,21 @@ readAudio(opts, function (err, stream) {
     for (var i = 0; i < audio.shape[0]; i++) {
       buf.push(audio.data[i])
     }
-    //console.log("audio", c++, audio)
+
     loop.update({
-      data: buf,
-      shape: bufShape
+      stroke: linearGradientToVsvg(
+        rainbowGradient({
+          start: start
+        })
+      ),
+      points: {
+        data: buf,
+        shape: bufShape
+      }
     })
 
-    nextTick(cb)
+    start += inc
+    cb()
   }))
 })
 
